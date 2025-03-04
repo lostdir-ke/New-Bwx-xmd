e44874a9ccf5b7b5580e0deef3a7fe03c32ab3fd
lostdir-ke <lostke254@gmail.com>
2025-03-04 15:26:01 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ const databasePath = path.join(__dirname, './database.db');
 const DATABASE_URL = process.env.DATABASE_URL === undefined
     ? databasePath
     : process.env.DATABASE_URL;
-module.exports = { session: process.env.SESSION_ID || 'BWM-XMD;;;H4sIAAAAAAAAA5VU25KqOBT9lam8yhm5C1Z11SAgYquIgKJT5yFAuCgChoCXLv99Cu2u7oc5Z3p4SiVh7ZW11t5voCizGr2iKxi+gQpnLSSoW5JrhcAQjJo4RhhQIIIEgiE4ao7bH6i3k+ndttpOWnObBe8FvHFwJ/U8RZN5YPUWeVAekhdwp0DVBHkW/gZwPO2HOKgtsWiX1l49zNT+muvLqcUpVRWv+FFCM9ki2uj+/AXcO0SY4axI9CpFR4Rh/oquS5jh79FnlyhZ7FcX04K+d9JYtg2ILRLX5zM79dfbbWDAkMNZwvLfo4+n7WqEvcTmD8ZiJ0GyKdOeHvK3lZVwzlUa02TWF2fCoKCf9OssKVBkRqggGbl+W3drsnW89tKSsXjyLLVN97uU2fPnlOPqnW6E/kAPsWWssHH+HnF6G0xPU7Gnj5LonPQa0m7UBomCuptZEcx66u4oO0WrzZX6K/El/sjK4f/oflSmqhuPOIXG+7yOlN5OVOeeXBUs4Rq9f2Who65q3T779jd1P1pHc5xE87UX89rrxhiJvqY4yT6YlIY/3k01LXW3lRpo9id9SBr8O5ZVINS5pN0syTlMZduDSb/U5akxCoi5Mm3LN/dVy8vxLdvWbLWMd1HFby7twDsYzm62VCw3q3SJWA26+bX/2vbmC6SdXx4vOqCrGYEhc6cARklWEwxJVhaPvYFMARi1DgoxIg95QWwp09TyrTix55r8WjHO2I9tJVAu/fUFx+ciS+MBcVbOdv4CKFDhMkR1jaJJVpMSX+eormGCajD8+ycFCnQhT+O6chxDgTjDNfGKpspLGH24+nEIw7BsCuJci1DtFgiDIf25jQjJiqTudGwKiMM0a5GaQlKDYQzzGt0pEKE2C1GHBy5jwZ2tB7ZTWK+x3E6mgnuj7Y5yWhbPKwEzkCRJin6wtBD94AUG/pBiPvrB8JAVIEPzbMgCCmTvPdP980sLRYbkY9fvO+5hs54Zwvninq4HbFbl9mHDU3uEUQSGBDeIAgEMD03llgdU/Aa3ns17TTvwmGW0Whk8oWnLDnxpaTLSF9ynp2D49jmn1DLq8KYqN5ktXAFQ4PiIYNa9nBX4Ac1L8kBihSEv/lX/ee6EhFX1Z4EIoEABu9tgkYVpmf/hojqHXbV3KzqcCBGY5TUYAtU0NsUy1HST7Scz3jAUPVHURAGf1n30wDNjAVqy7ZyRe6tR73wQsrnKCKNV2KDilRNDj1uvmxHrwlbyzZd/AelelRfTRS/oB6+FHpyuTr1UXZG7ihy+sQU8lZ6+8jSV2USK0pr0dilDVxerZjtRBLlghSLn2kHWO3kTruJjUkxMe9FoyktX7Rmhr8UWjSG3F1PhkzaP5mMj2G03u402o0eniaKrcDZm9+NlE3kxTqp4l9Kn+qyt4i3arf3QKLekP/Nch9Pd5lSJm2VfE+Slojy78zEd8vepnD365u09cHGGHkPu3Yr/tOwz+/Sd+oLxPjZ/ka/RhtvL+LaI44Md11Nru1cIsj1/VlRX0ivGC2YdJNGBxf3xFdzvPylQ5ZDEJT524TwGEFAghzVRPnvXzY6oJvBYdROGp0WeZwWGAserUlUOgeSj5YHSfROrBfd/AL1JxeMOCAAA',
+module.exports = { session: process.env.SESSION_ID || 'BWM-XMD;BWM-XMD;;;H4sIAAAAAAAAA5VU2ZKqSBT8lYl61TvKKhjREcMiIm4gqI0T96GEAopdKETs8N8n6CW6H+be6eGpoqrIk5WZ57yAvMA1WqIOTF9AWeErJKhfkq5EYArkJghQBYbAhwSCKRAMZ0Wk1HMzZ6vveIuz7PAkMJEVisKYPouem4drXhihxH0CjyEom3OKvd8AarrNc3cxZBpndBcZU1nB7UnLNytpczftOhK2IhPVHatqsyfw6BEhrnAezsoIZaiC6RJ1JsTV9+hny9g6zU5srTSjbF9k/M1teEtngpRr2vnh4Ppda09WnDzef4/+WSlpL6CJfkwbywz8RF65ecupNqzCfXiRVd2v9QU34DT3jX6Nwxz5Cx/lBJPu27q30jZSOQbHvnyi4nm4iUvtVNVxzI4O2lZS9tGoM2Vj1unS94if5jIVbRbl7s7GzEo7DrY3h96L5crKeNW0SrjzKJuYNzRjvxI3q4+sJP9L96180UbyOFbTjl8o7TM0LrVwjrirONBTJ6wilm8bzsjy4nv0J+nO8B23nosx4c+nwWyH7mZjn6/rJOSeOWWVUYxRsXlDC5/0IWmq37FkIt61Z/RC2cuhQRVuItXqWjW0QW6y1OFISpuz28TZuM7RDXDQrlbj5KKXx3tFre6pbh3F3Wx3qal1Eu+3aebLlBdj6en1RQnqFj6YUo8hqFCIa1JBgou83+OGAPpXG3kVIq/iAqMwXC8xL+vrQImx3rHHucWo+uzeOWXVxWfuGXWpkwnRQngCQ1BWhYfqGvk6rklRdWtU1zBENZj+/XMIcnQjb7b1xRhqCAJc1WSfN2VaQP/D049D6HlFkxO7yz2lX6AKTMef24gQnId1r2KTw8qL8BUpESQ1mAYwrdFjCHx0xR7q8YBzjm5CxbqWiLMDnKjpRKY7q6ccFfnbFeHM+743pn+IDEv9YGna+yFATvgh+p4fTM4s5wkiGAL83jH9P7808BbpzXy2M1rbSjiXmnmsv58szGLWJq8mvCmPKuSDKakaNARn6CVN6RQJyn+Dm2TNZiJ5+iY+hsRkOmZyRTfZhCd1/AX3zVEwffmcUkrh93hLc36wZNsGQ5C9BhD3L6c5djJmBXEi0NyUnfxV/9n2QsKy/DNHBAxBDvvbYIO9qEj/cFCdwr7auxU9jo8IxGkNpkBZLI+56amzlZIlZ3Y+l2ahpIQS+LTuowPeMnZGJn1dU+JgJw/ahMNrheLkndegfMnw3p45HBqZduBVeF48/QsImAJ+4NxVo1tul/tmA4keBXy7sG7L1cqO3P3F4+KzjnXtdtWfDWpQXcVnJm7FdpyGfFB30k1YCrRRbaObwyCdueHq6DNy+NRXe4vQ12L5RcWaUSnMdVFd91o6SphNfEqKsRtymlUegkbdqocZuy4NtLM7KqkDWo03cOFfRSfwmdOdES0Ju9plVHolzwsmPsbtW2++zob0fSbj1755eQ9cgNHriHu34j8t+8z++DH8gvE+NH+RL/nIxGJ13wRBYgW1sXVjiSBr/7zKy44Mcm1DHc6hn9DVSOvA4/FzCMoUkqCoMjAFdXaGYAhSWBPps3cdnKGawKwEU2rCjkVK5Cl2CLJOKkubQPLR8kDqP317B49/AI/2cTkMCAAA',
     PREFIXE: process.env.PREFIX || ".",
-    OWNER_NAME: process.env.OWNER_NAME || "Ibrahim Adams",
+    OWNER_NAME: process.env.OWNER_NAME || "NICHOLAS ↪️TREKKER2",
     NUMERO_OWNER : process.env.NUMERO_OWNER || "254704897825",              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'no',
-    BOT : process.env.BOT_NAME || 'BMW_MD',
+    BOT : process.env.BOT_NAME || 'TREKKER2',
     URL : process.env.BOT_MENU_LINKS || 'https://files.catbox.moe/h2ydge.jpg',
     MODE: process.env.PUBLIC_MODE || "yes",
     PM_PERMIT: process.env.PM_PERMIT || 'yes',
@@ -26,14 +26,14 @@ module.exports = { session: process.env.SESSION_ID || 'BWM-XMD;;;H4sIAAAAAAAAA5V
     ANTIDELETE1 : process.env.ANTIDELETE1 || 'yes',
     ANTIDELETE2 : process.env.ANTIDELETE2 || 'yes',
     MENUTYPE : process.env.MENUTYPE || '',
-    ANTICALL : process.env.ANTICALL || 'yes',
+    ANTICALL : process.env.ANTICALL || 'NO',
                   AUTO_REACT : process.env.AUTO_REACT || 'yes',
                   AUTO_REACT_STATUS : process.env.AUTO_REACT_STATUS || 'yes',
                   AUTO_REPLY : process.env.AUTO_REPLY || 'no',
                   AUTO_READ : process.env.AUTO_READ || 'yes',
                   AUTO_SAVE_CONTACTS : process.env.AUTO_SAVE_CONTACTS || 'yes',
                   AUTO_REJECT_CALL : process.env.AUTO_REJECT_CALL || 'no',
-                  AUTO_BIO : process.env.AUTO_BIO || 'yes',
+                  AUTO_BIO : process.env.AUTO_BIO || 'NO',
                   AUDIO_REPLY : process.env.AUDIO_REPLY || 'no',
     DATABASE_URL,
     DATABASE: DATABASE_URL === databasePath
